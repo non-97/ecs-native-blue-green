93cf6a5c5f6117c5a418e020626c7caeb402af81
non-97 <yamamoto.ryota@classmethod.jp>
2026-01-05 18:34:17 +0900

    Initial commit

---
+++ test/ecs-native-blue-green.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsNativeBlueGreen from '../lib/ecs-native-blue-green-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-native-blue-green-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsNativeBlueGreen.EcsNativeBlueGreenStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
